d758dea38bce890c58efe1ba903acdbe0a3e6220
Aliyah Jessa <aliyah@aliyahmaliyah.com>
2016-02-01 20:47:21 +0000

    Distance from ISS

--- iss.js
+++ iss.js
@@ -1,8 +1,43 @@
+Number.prototype.toRadians = function() {
+    return this * Math.PI / 180;
+};
 var request = require("request");
-var iss = "http://api.open-notify.org/iss-now.json";
-
-request(iss, function(req,data){
-    var issData = JSON.parse(data.body);
-    console.log("Current longitude:" + (Math.round((issData.iss_position.longitude)*100)/100) + "Current latitude:" + (Math.round((issData.iss_position.latitude)*100)/100));
-    //console.log("Current longitude:" + issData.iss_position.longitude + "Current latitude:" + issData.iss_position.latitude);
+var prompt = require("prompt");
+prompt.start();
+prompt.get(["city"], function(err,result){
+    if (err) {
+        return "Error";
+    }
+    var personLatLon = "https://maps.googleapis.com/maps/api/geocode/json?address=" + result.city;
+    request(personLatLon, function(req,data){
+        var personLatLonData = JSON.parse(data.body);
+        var lat1 = personLatLonData.results[0].geometry.location.lat;
+        var lon1 = personLatLonData.results[0].geometry.location.lng;
+        var iss = "http://api.open-notify.org/iss-now.json";
+        request(iss, function(req,data){
+            var issData = JSON.parse(data.body);
+            var lon2 = issData.iss_position.longitude;
+            var lat2 = issData.iss_position.latitude;
+            var distance = calcDifference(lon1, lon2, lat1, lat2);
+            console.log(distance)
 });
+    })});
+     
+function calcDifference(lon1, lon2, lat1, lat2){
+    var R = 6371000; // metres
+    var φ1 = lat1.toRadians();
+    var φ2 = lat2.toRadians();
+    var Δφ = (lat2-lat1).toRadians();
+    var Δλ = (lon2-lon1).toRadians();
+    
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    
+    var d = R * c;
+    return d;
+};
+
+
+
